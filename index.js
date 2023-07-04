26cb980af595d5e10c3b76e92add45f721962911
ashish.chodankar <ashish.chodankar@squareyards.co.in>
2023-07-04 17:18:57 +0530

    Store created and actions dispatched;implements issue #3

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const redux = require("redux"); //1.Holds application state //import redux
+const createStore = redux.createStore; //1.Holds application state //create store
 const BUY_CAKE = "BUY_CAKE";
 
 function buyCake() {
@@ -7,6 +9,7 @@ function buyCake() {
   };
 }
 //Action is with the type property and action creater is a function with the type property
+
 //Reducer:
 //(previousState,action)=>newState
 
@@ -25,3 +28,11 @@ const reducer = (state = initialState, action) => {
       return state;
   }
 };
+
+const store = createStore(reducer); //1.Holds application state
+console.log("Initial state", store.getState()); //2.Allow access to state via getState() i.e. here provides initial state of app because state is not updated
+const unsubscribe = store.subscribe(() => console.log("Updated state", store.getState())); //subsribe changes in the store//4.registers listeners via subscribe
+store.dispatch(buyCake()); //3.Allow state to be updated via dispatch
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+unsubscribe(); //5.handle unregistering of listeners
